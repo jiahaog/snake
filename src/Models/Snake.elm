df4f878b309d4da80ae50de2bee28da428527d30
Goh Jia Hao <jiahaog@gmail.com>
2016-11-05 22:12:34 +0800

    Implement function to move snake - Change function signature of `growSnake` for better currying

--- src/Models/Snake.elm
+++ src/Models/Snake.elm
@@ -25,7 +25,7 @@ snakeInitGenerator =
 
 newSnake : Coordinate -> Direction -> Snake
 newSnake head direction =
-    growSnake [ head ] direction
+    growSnake direction [ head ]
 
 
 maybeGrowSnake : Snake -> (Coordinate -> Coordinate) -> Maybe Coordinate -> Snake
@@ -38,7 +38,32 @@ maybeGrowSnake snake whereToPut maybeHead =
             []
 
 
-growSnake : Snake -> Direction -> Snake
-growSnake snake direction =
+growSnake : Direction -> Snake -> Snake
+growSnake direction snake =
     List.head snake
         |> maybeGrowSnake snake (coordinateOffset direction)
+
+
+maybeWithoutTail : Maybe Snake -> Snake
+maybeWithoutTail maybeSnake =
+    case maybeSnake of
+        Just withoutTail ->
+            List.reverse withoutTail
+
+        Nothing ->
+            []
+
+
+removeSnakeTail : Snake -> Snake
+removeSnakeTail snake =
+    snake
+        |> List.reverse
+        |> List.tail
+        |> maybeWithoutTail
+
+
+moveSnake : Direction -> Snake -> Snake
+moveSnake direction snake =
+    snake
+        |> removeSnakeTail
+        |> growSnake direction
